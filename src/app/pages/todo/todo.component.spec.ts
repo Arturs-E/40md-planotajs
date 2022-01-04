66b6aca9b56a304698e2c338f68923f4a32e0a5d
Arturs Endzelins <arturs.endzelins@gmail.com>
2022-01-04 09:42:46 +0200

    Added user route unsubscription on destroy lifecycle

--- src/app/pages/todo/todo.component.spec.ts
+++ src/app/pages/todo/todo.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { TaskComponent } from './task/task.component';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
+  let taskComponent: TaskComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let taskFixture: ComponentFixture<TaskComponent>;
+  let sampleDataElement: HTMLSpanElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [
+        TodoComponent,
+        TaskComponent
+      ]
     })
     .compileComponents();
   });
@@ -17,9 +24,21 @@ describe('TodoComponent', () => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    taskFixture = TestBed.createComponent(TaskComponent);
+    taskComponent = taskFixture.componentInstance;
+    sampleDataElement = taskFixture.nativeElement.querySelector('.todo__task-item-task');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have heading of To-do list', () => {
+    const heading: HTMLHeadingElement = taskFixture.nativeElement.querySelector('h2');
+    expect(heading.textContent).toBe('To-do list');
+  });
+
+  it('should include sample task - Complete homework', () => {
+    expect(sampleDataElement.textContent).toBe('Complete homework');
   });
 });
